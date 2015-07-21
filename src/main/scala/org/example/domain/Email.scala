9343b69c69e2203af950ea354d7ea1d261540ea3
Piyush Purang <ppurang@gmail.com>
2015-07-21 15:50:17 +0200

    introduce \/

--- src/main/scala/org/example/domain/Email.scala
+++ src/main/scala/org/example/domain/Email.scala
@@ -1,9 +1,25 @@
 package org.example.domain
 
-case class Email(email: String) {
-  require(Email.emailRegex.findFirstIn(email).isDefined, "ewwwww")
-}
+import scalaz.\/
+import scalaz.syntax.either._
+
+case class Email(email: String)
 
 object Email {
-  private val emailRegex = """[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}""".r
+
+  private val emailRegex = {
+    """[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}""".r
+  }
+
+  // \/[String, Email] is the same as  'String OR Email'
+
+  implicit def validate: Email => \/[String, Email] = email => {
+
+    emailRegex.findFirstIn(email.email) match {
+      case Some(_) => email.right
+      case _ => s"Not a valid email $email".left
+    }
+
+  }
+
 }
